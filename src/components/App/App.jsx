df098be955df3efb7e17706a790f497df59e8ce2
Gogolbog <111005996+Gogolbog@users.noreply.github.com>
2023-03-25 16:47:21 +0200

    done

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -3,6 +3,9 @@ import ApiSearchImg from 'services/getImg';
 import Searchbar from '../Searchbar/Searchbar';
 import { AppWrapper } from './AppStyled';
 import Notiflix from 'notiflix';
+import { ImageGallery } from 'components/ImageGallery/Imagegallery';
+import { Btn } from 'components/Button/btn';
+import { Loader } from 'components/Loader/Loader';
 
 const apiSearchImg = new ApiSearchImg();
 
@@ -10,39 +13,62 @@ class App extends Component {
   state = {
     searchValue: '',
     images: [],
+    isLoading: false,
+    showBtn: false,
   };
 
   componentDidUpdate(_, prevState) {
     if (prevState.searchValue !== this.state.searchValue) {
-    }
-    apiSearchImg
-      .getImg(this.state.searchValue)
-      .then(data => {
-        if (data.hits.length === 0) {
-          return Notiflix.Notify.failure(
-            'Sorry, there are no images matching your search query. Please try again.'
-          );
-        }
-        this.setState({
-          images: data.hits,
+      this.setState({ isLoading: true });
+      apiSearchImg.resetPage();
+      apiSearchImg
+        .getImg(this.state.searchValue)
+        .then(data => {
+          if (data.hits.length === 0) {
+            return Notiflix.Notify.failure(
+              'Sorry, there are no images matching your search query. Please try again.'
+            );
+          }
+          const galleryLength = data.hits.length < 12;
+          this.setState({ showBtn: galleryLength ? false : true });
+          this.setState({
+            images: data.hits,
+          });
+        })
+        .catch(error => {
+          console.log(error);
+        })
+        .finally(() => {
+          this.setState({ isLoading: false });
         });
-      })
-      .catch(error => {
-        console.log(error);
-      })
-      .finally(() => {
-        console.log(this.state.images);
-      });
+    }
   }
 
   onSubmit = searchValue => {
     this.setState({ searchValue });
   };
 
+  onClick = async () => {
+    try {
+      await apiSearchImg.incrementPage();
+      const data = await apiSearchImg.getImg(this.state.searchValue);
+      this.setState(prevState => ({
+        images: [...prevState.images, ...data.hits],
+      }));
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
   render() {
+    const { showBtn, images, isLoading } = this.state;
+
     return (
       <AppWrapper>
         <Searchbar onSubmit={this.onSubmit} />
+        <ImageGallery galleryArr={images} />
+        {isLoading && <Loader />}
+        {showBtn && <Btn onClick={this.onClick} />}
       </AppWrapper>
     );
   }
--- src/components/ImageGallery/Imagegallery.jsx
+++ src/components/ImageGallery/Imagegallery.jsx
@@ -1,5 +1,18 @@
 import { Ul } from './ImageGalleryStyled';
+import PropTypes from 'prop-types';
+import ImageGalleryItem from 'components/ImageGalleryItem/ImageGalleryItem';
 
-export const ImageGallery = ({ images }) => {
-  return <Ul></Ul>;
+export const ImageGallery = ({ galleryArr }) => {
+  return (
+    <Ul>
+      {galleryArr.map(item => (
+        <ImageGalleryItem
+          key={item.id}
+          webformatURL={item.webformatURL}
+          largeImageURL={item.largeImageURL}
+          tags={item.tags}
+        />
+      ))}
+    </Ul>
+  );
 };
